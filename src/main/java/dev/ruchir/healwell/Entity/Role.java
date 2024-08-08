65df479d545e0f61da228fe1badd23fa4ee14ee0
Ruchir Mandora <ruchirm702@gmail.com>
2024-08-08 22:38:57 +0530

    Added more entities

---
+++ src/main/java/dev/ruchir/healwell/Entity/Role.java
@@ -0,0 +1,18 @@
+package dev.ruchir.healwell.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
---
+++ src/main/java/dev/ruchir/healwell/Entity/User.java
@@ -0,0 +1,29 @@
+package dev.ruchir.healwell.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
