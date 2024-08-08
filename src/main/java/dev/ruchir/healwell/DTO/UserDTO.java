64bd86e7f96a9338beecb26e5e43583d81c9b3b0
Ruchir Mandora <ruchirm702@gmail.com>
2024-08-08 22:52:13 +0530

    Added another DTO

---
+++ src/main/java/dev/ruchir/healwell/DTO/UserDTO.java
@@ -0,0 +1,12 @@
+package dev.ruchir.healwell.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String username;
+    private String password;
+}
